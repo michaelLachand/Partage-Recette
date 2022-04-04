3903f641638f7cc923a00e210ea7fc08652b2ac5
wes35 <lachand_michael@hotmail.com>
2022-04-04 17:26:53 +0200

    add fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 1; $i <= 50; $i++){
+            $ingredient = new Ingredient();
+            $ingredient->setName($faker->word())
+                ->setPrice(mt_rand(0, 100));
+
+            $manager->persist($ingredient);
+        }
+
+
+        $manager->flush();
+    }
+}
