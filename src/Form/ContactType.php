9d23595f09da6a6ed1a9bdccc0d9f4271fef723b
wes35 <lachand_michael@hotmail.com>
2022-05-16 14:44:45 +0200

    installation test

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Contact;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -77,7 +79,10 @@ class ContactType extends AbstractType
                 ],
                 'label' => 'Envoyer le message'
             ])
-        ;
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
